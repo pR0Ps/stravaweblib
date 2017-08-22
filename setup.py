27e5ec783a5ed9275ae05ca311405f8079e431f5
Carey Metcalfe <carey@cmetcalfe.ca>
2017-08-22 10:21:35 -0400

    Add more trove classifiers
    From https://pypi.python.org/pypi?%3Aaction=list_classifiers

--- setup.py
+++ setup.py
@@ -8,11 +8,17 @@ setup(name="stravaweblib",
       url="https://github.com/pR0Ps/stravaweblib",
       license="MPLv2",
       classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+          "Operating System :: OS Independent",
           "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3 :: Only",
           "Programming Language :: Python :: 3.3",
           "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
+          "Topic :: Software Development :: Libraries :: Python Modules"
       ],
       packages=["stravaweblib"],
       install_requires=["stravalib>=0.6.6,<1.0.0", "beautifulsoup4>=4.6.0,<5.0.0"]
